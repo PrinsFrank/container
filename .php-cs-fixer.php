71f64d3e2a2fb072464fefacdc476a3a550610a1
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2024-06-18 19:51:06 +0200

    Scaffold PHP package files

---
+++ .php-cs-fixer.php
@@ -0,0 +1,28 @@
+<?php
+
+return (new PhpCsFixer\Config())->setRules(
+    [
+        '@PSR12'                       => true,
+        'declare_strict_types'         => true,
+        'strict_comparison'            => true,
+        'strict_param'                 => true,
+        'blank_line_after_opening_tag' => false,
+        'no_extra_blank_lines'         => true,
+        'class_attributes_separation'  => ['elements' => ['const' => 'only_if_meta', 'method' => 'one', 'property' => 'only_if_meta', 'trait_import' => 'only_if_meta', 'case' => 'only_if_meta']],
+        'array_syntax'                 => ['syntax' => 'short'],
+        'binary_operator_spaces'       => ['default' => 'single_space'],
+        'no_unused_imports'            => true,
+        'array_indentation'            => true,
+        'no_alias_functions'           => true,
+        'phpdoc_line_span'             => ['property' => 'single', 'method' => 'single', 'const' => 'single'],
+        'phpdoc_order'                 => ['order' => ['deprecated', 'internal', 'standard', 'source', 'template', 'implements', 'extends', 'covers', 'coversDefaultClass', 'param', 'throws', 'return']],
+        'phpdoc_param_order'           => true,
+        'phpdoc_separation'            => ['groups' => [['deprecated', 'internal'], ['standard', 'source'], ['covers', 'coversDefaultClass'], ['template', 'implements', 'extends', 'param', 'throws', 'return']]],
+        'phpdoc_trim'                  => true,
+        'concat_space'                 => ['spacing' => 'one'],
+    ]
+)->setFinder(
+    PhpCsFixer\Finder::create()
+        ->exclude('vendor')
+        ->in(__DIR__)
+);
