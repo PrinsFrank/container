dce1b9e824db5b66f4fd30c199f64b71133fcf0f
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-07-06 12:40:17 +0200

    Add missing override and throws annotations

--- src/ServiceProvider/ContainerProvider.php
+++ src/ServiceProvider/ContainerProvider.php
@@ -2,15 +2,20 @@
 
 namespace PrinsFrank\Container\ServiceProvider;
 
+use Override;
 use PrinsFrank\Container\Container;
 use PrinsFrank\Container\Definition\DefinitionSet;
 use PrinsFrank\Container\Definition\Item\Singleton;
+use PrinsFrank\Container\Exception\InvalidArgumentException;
 
 class ContainerProvider implements ServiceProviderInterface {
+    #[Override]
     public function provides(string $identifier): bool {
         return $identifier === Container::class;
     }
 
+    /** @throws InvalidArgumentException */
+    #[Override]
     public function register(DefinitionSet $resolvedSet): void {
         $resolvedSet->add(
             new Singleton(
